b3196fe9adebf146c29be1930025c9a488ec03ff
Dmitry <darkmilkman@gmail.com>
2023-02-27 21:36:14 +0300

    feat: success

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor(){}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this.min)/2);
     }
 
     lower() {
-
+        return this.max = Math.round((this.max + this.min)/2);
     }
 
     greater() {
-
+        return this.min = Math.round((this.max + this.min)/2);
     }
 }
 
